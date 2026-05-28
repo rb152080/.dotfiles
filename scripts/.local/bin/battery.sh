fffb92305215f31d986df497f051aa4ef9d7a777
rb152080 <rishib2581@gmail.com>
2026-05-28 19:53:30 -0400

    added scripts

---
+++ scripts/.local/bin/battery.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
+WARNING_LEVEL=25
+BATTERY_DISCHARGING=$(acpi -b | grep -c "Discharging")
+BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+
+if [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ]; then
+	notify-send "LOW BATTERY"
+fi
---
+++ scripts/.local/bin/displays.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+INTERNAL="eDP-1"
+EXTERNAL="HDMI-1-0"
+MODE="1920x1080"
+RATE=60
+
+chosen=$(printf "secondary\nright\nleft\nprimary\nmirror" | rofi -dmenu)
+
+case "$chosen" in
+"secondary")
+	xrandr --output $EXTERNAL --primary --mode $MODE --rate $RATE --output $INTERNAL --off
+	;;
+"right")
+	xrandr --output $INTERNAL --primary --mode $MODE --rate $RATE --output $EXTERNAL --right-of $INTERNAL --mode $MODE --rate $RATE
+	;;
+"left")
+	xrandr --output $INTERNAL --primary --mode $MODE --rate $RATE --output $EXTERNAL --left-of $INTERNAL --mode $MODE --rate $RATE
+	;;
+"primary")
+	xrandr --output $INTERNAL --primary --mode $MODE --rate $RATE --output $EXTERNAL --off
+	;;
+"mirror")
+	xrandr --output $INTERNAL --primary --mode $MODE --rate $RATE --output $EXTERNAL --same-as $INTERNAL
+	;;
+esac
