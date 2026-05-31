3b45eaf2b73f41366568b7d17a1be979d0eae35d
rb152080 <rishib2581@gmail.com>
2026-05-30 20:37:19 -0400

    setting up token

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,19 +2,15 @@ return {
 	"rb152080/flamingo.nvim",
 	name = "flamingo.nvim",
 	config = function()
-		if os.getenv("DISPLAY") == nil then
-			vim.cmd("colorscheme default")
-		else
-			require("flamingo").setup({
-				styles = {
-					italic = false,
-					bold = false,
-					transparency = false,
-				},
-			})
-			vim.cmd("colorscheme flamingo")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end
+		require("flamingo").setup({
+			styles = {
+				italic = false,
+				bold = false,
+				transparency = false,
+			},
+		})
+		vim.cmd("colorscheme flamingo")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
